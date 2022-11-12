5d17665f955b2a95d3a1c62dadf7af553729f2b9
Ayelet Hillel <ah3708@tc.columbia.edu>
2022-11-11 22:05:43 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ app.get(`/tip/:total/:tipPercentage`, (request, response) => {
     response.send(`The tip is ${(percentage/100)*total}`)
 })
 
+//Magic 8 Ball
+
+const magicBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get(`/magic/:question`, (request, response) => {
+    var answer = magicBall[Math.floor(Math.random()*magicBall.length)]
+    response.send(`<h1>${request.params.question}? <br> ${answer}</h1>`)
+
+})
+
+
 
 app.listen(3000, () => {
     console.log(`listening to port 3000`);
